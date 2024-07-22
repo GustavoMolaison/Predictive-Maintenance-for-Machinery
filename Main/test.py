28bc710bce73b3e27247653af9267030ad5d2d6c
GustavoMolaison <hubertwolowiec1102@gmail.com>
2024-07-22 21:28:28 +0200

    cool

--- Main/test.py
+++ Main/test.py
@@ -7,7 +7,18 @@ pd.set_option('display.max_rows', 100)
 pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', None)
 
-df_test1 = pd.read_csv('test_FD001.txt')
-df_train1 = pd.read_csv('train_FD001.txt')
+df_test1_fake = pd.read_csv('test_FD001.txt')
+df_train1_fake = pd.read_csv('train_FD001.txt')
 
-print(df_test1.index)
+
+
+print(df_test1_fake.columns)
+bef_split = df_test1_fake.columns
+bef_split = str(bef_split)
+column_test1 = bef_split.split() 
+
+
+df_test1 = pd.DataFrame( {'unit number' : [column_test1[0]]} )
+print(column_test1[3])
+
+
