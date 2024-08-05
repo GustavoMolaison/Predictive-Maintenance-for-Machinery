fbe3d5b8e8fa7797d23ca7bd1cbe8904e16e67b6
GustavoMolaison <hubertwolowiec1102@gmail.com>
2024-08-05 18:44:18 +0200

    sda

--- Main/testss.py
+++ Main/testss.py
@@ -1,2 +1,21 @@
+import numpy as np
 import tensorflow as tf
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+
+print("TensorFlow version:", tf.__version__)
+
+# Sample data
+X_train = np.random.rand(100, 10, 1)
+y_train = np.random.rand(100, 1)
+
+# Define the model
+model = Sequential()
+model.add(LSTM(50, activation='relu', input_shape=(10, 1)))
+model.add(Dense(1))
+
+# Compile the model
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+# Train the model
+model.fit(X_train, y_train, epochs=5, verbose=1)
