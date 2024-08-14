39fc9d17a9a7c67e53e144be7b5ae68813b05404
GustavoMolaison <hubertwolowiec1102@gmail.com>
2024-08-14 23:18:09 +0200

    neie moge kurwa kdsajoasdpksadasdxzdaxz

--- Main/test.py
+++ Main/test.py
@@ -1,2 +1,19 @@
-import tensorflow.keras
-print(tensorflow.keras.__version__)
+import numpy as np
+# xd = []
+# xdd = []
+# papa =(np.array([[2,2,2],
+#                      [1,1,1]]))
+# xdd.append(papa)
+# patrz = xdd
+# xd.append(patrz)
+# xdd.clear()
+# print(xd[0][0])
+list1 = []
+list2 = []
+for g in range(10):
+  list1 = []
+  for i in range(10):
+    num = i
+    list1.append(num)
+  list2.append(list1)
+print(list2)    
