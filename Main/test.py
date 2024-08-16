776b457146e6322b0f2e7a8519bf3b8c172146e3
GustavoMolaison <hubertwolowiec1102@gmail.com>
2024-08-16 20:39:47 +0200

    asd

--- Main/test.py
+++ Main/test.py
@@ -8,12 +8,15 @@ import numpy as np
 # xd.append(patrz)
 # xdd.clear()
 # print(xd[0][0])
-list1 = []
-list2 = []
-for g in range(10):
-  list1 = []
-  for i in range(10):
-    num = i
-    list1.append(num)
-  list2.append(list1)
-print(list2)    
+# list1 = []
+# list2 = []
+# for g in range(10):
+#   list1 = []
+#   for i in range(10):
+#     num = i
+#     list1.append(num)
+#   list2.append(list1)
+# print(list2)    
+y = 20
+for x in range(1, y):
+    print(x)
