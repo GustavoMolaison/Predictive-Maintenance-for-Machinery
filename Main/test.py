a633a0dd164a6ecdc79aee56f83b22e1b6658509
GustavoMolaison <hubertwolowiec1102@gmail.com>
2024-08-21 21:36:12 +0200

    sadads

--- Main/test.py
+++ Main/test.py
@@ -8,6 +8,31 @@ import numpy as np
 # print(f'new array{array1}')
 # ys = np.delete(ys, 0, axis = 0)
 
-array = np.ones([1, 5, 10])
-array = np.vstack((array, np.ones([1, 5, 10]))) 
+array = np.ones([1, 3, 5, 10])
+
+array2 = np.ones([15, 10])
+
+array2 = array2.reshape([1, 3, 5, 10])
+
+array = np.vstack((array, array2))
 print(array)
+print('XDDDDDDDDDDDDDDD')
+array = array.reshape([3,5,10])
+
+# array3 = np.ones([1, 1, 5, 10])
+
+# array3 = np.vstack((array3, array))
+# print('XDDDDDDDDDDDDDDDDDDDDDDDDDD')
+# print(array3)
+
+
+
+# array = np.ones([1, 10, 148])
+
+# array2 = np.ones([10, 148])
+
+# array2 = array2.reshape([1, 10, 148])
+
+# array = np.vstack((array, array2))
+
+# print(array.shape)
